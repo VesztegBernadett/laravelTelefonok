d5e8c16553d36f0811d2af2ee3971861f6c5a105
VesztegBernadett <vdetti2004@gmail.com>
2024-10-07 12:17:08 +0000

    phone list created

---
+++ backend/resources/views/phone/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.main')
+
+@section("title",$title)
+
+@section("content")
+<div class="container">
+    <h1>{{$title}}</h1>
+
+    <div class="row align-items-stretch">
+        @foreach($phones as $phone)
+        <div class="col-md-6 col-lg-3 py-3">
+            <div class="card h-100">
+                <img src="{{asset("img/" . $phone['id'] . ".jpg")}}" class="card-img-top" alt="{{$phone['manufacturer']}} {{$phone['model']}}">
+                <h5 class="card-title">{{$phone['manufacturer']}} {{$phone['model']}}</h5>
+                <p class="card-text">{{$phone['description']}}</p>
+                <div class="card-footer">
+                    <div class="row">
+                        <div class="col-md-8 d-grid">
+                            <a class="btn btn-primary"
+                                href="{{route("phones.show",["id"=> $phone["id"]])}}">
+                            {{$phone['manufacturer']}} {{$phone['model']}}
+                            </a>
+                        </div>
+                        <div class="col-md-4">
+                            <span>{{number_format($phone["price"], 0, ",", " ")}}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+@endsection
